87242dd625176dbaf8ae1da76d1e417f8b610f0f
Laharikrkv <laharik6.24@gmail.com>
2025-10-05 17:03:30 +0530

    Initial commit

--- src/main/java/com/example/lahari/controller/PersonControllerImpl.java
+++ src/main/java/com/example/lahari/controller/PersonControllerImpl.java
@@ -3,7 +3,6 @@ package com.example.lahari.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,7 +44,7 @@ public class PersonControllerImpl implements PersonController{
 	
 
 	@Override
-	@PutMapping("{personId}")
+	@PutMapping("/{personId}")
     public ResponseEntity<PersonDto> updatePerson(@PathVariable Long personId, @RequestBody PersonDto updatedPerson){
 		PersonDto personDto = personService.updatePerson(personId, updatedPerson);
 		return ResponseEntity.ok(personDto);
